179122e55f188c7b992bf8dec64761757a0aec63
SHYAMGOPAL BISWAS <shyamgopalbiswas22114@gmail.com>
2024-02-14 21:45:19 +0530

    fullstack

---
+++ Full Stack CRUD/Notes_App/model/user.model.js
@@ -0,0 +1,16 @@
+
+const mongoose=require("mongoose")
+
+const userSchema=mongoose.Schema({
+    username:{type:String, required:true},
+    email:{type:String,required:true},
+    password:{type:String,required: true}
+},{
+    versionKey:false
+})
+
+const UserModel = mongoose.model("notesapp",userSchema)
+
+module.exports={
+    UserModel
+}
